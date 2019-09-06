e9aa308b04125b9deb8f9443860af41434a665f2
kholvoet <kwh@mournival.com>
2019-09-06 09:00:55 -0400

    Refactored PreComputations to remove default args (test impl artifacts)

--- src/pre-computations.ts
+++ src/pre-computations.ts
@@ -1,13 +1,13 @@
 import {position, Ray} from './ray';
 import {Intersection} from './intersection';
-import {point, Tuple, vector} from './tuple';
-import {normal_at} from "./sphere";
+import {Tuple} from './tuple';
+import {normal_at} from './sphere';
 
 export class PreComputations extends Intersection {
     constructor(public readonly i: Intersection,
-                public readonly p: Tuple = point(0, 0, 0),
-                public readonly eyev = vector(0, 0, 0),
-                public readonly normalv = vector(0, 0, 0)
+                public readonly p: Tuple,
+                public readonly eyev: Tuple,
+                public readonly normalv: Tuple
     ) {
         super(i.obj, i.t);
     }
@@ -19,8 +19,10 @@ export class PreComputations extends Intersection {
 }
 
 export function prepare_computations(i: Intersection, r: Ray): PreComputations {
-    const p = position(r, i.t);
-    const e = r.direction.negative;
-    const n = normal_at(i.obj, p);
-    return new PreComputations(i, p, e, n);
+    return new PreComputations(
+        i,
+        position(r, i.t),
+        r.direction.negative,
+        normal_at(i.obj, position(r, i.t))
+    );
 }
